2fce34a7b16f3b1f9ee836a754d00d3da2d80d22
aliraza13793 <aliraza13793@gmail.com>
2024-05-11 02:36:44 +0500

    update

---
+++ src/components/input.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Input() {
+  return (
+    <div className='text-center justify-center'>
+      <input type="search" placeholder='Search Here' className='bg-gray-600 hover:bg-violet-500 active:bg-violet-500 focus:outline-none focus:ring focus:ring-gray-300 rounded-full h-14 w-1/3 p-5' />
+      </div>
+  )
+}
+
+export default Input
